30b891aca885c66cdc4f2c78faf25c578e87801f
dandres0204 <daan_andres@hotmail.com>
2022-07-09 23:21:10 -0500

    primera entrega

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-let name = (prompt("Ingrese su nombre:"));
-
+let nombre = (prompt("Ingrese su nombre:"));
 let edad = parseInt(prompt("Ingrese su Edad:"));
 
+const name = nombre
+
 while (edad < 18) {
-    alert(name + " " + "Usted es menor de edad, pida ayuda a un Adulto");
+    alert(nombre + " " + "Usted es menor de edad, pida ayuda a un Adulto");
     break
 }
 let total_pagar = 0;
@@ -14,6 +15,8 @@ for (let i=0; i<cant_objets; i++) {
     let nombreobjects = prompt("Ingrese el Nombre del objeto:");
     let precio = parseFloat(prompt("Ingrese el Precio:"));
     total_pagar += precio;
+
+
     let mensaje = "Ingresaste: " + nombreobjects + " por $" + precio;
     alert(mensaje);
     console.log(mensaje);
@@ -22,60 +25,109 @@ for (let i=0; i<cant_objets; i++) {
         alert("Has superado el valor de compras máximas este mes!");
         break;
     }
-
 }
-let mensaje_final = "El total a pagar sin iva es: $" + total_pagar;
-alert(mensaje_final);
-console.log(mensaje_final);
+let mensaje = "El total a pagar sin iva es: $" + total_pagar;
+alert(mensaje);
+console.log(mensaje);
 
-let ivacol = a => a * 0.19; 
-let iva = ivacol(total_pagar)
+function operaciones(operacion){
+    if(operacion === "suma"){
+        return (x, y) => x + y;
+    }
+    if(operacion === "tax"){
+        return a => a * 0.19;
+    }
+    if(operacion === "division"){
+        return (a,b) => a / b;
+    }
+    if(operacion === "interes"){
+        return  a => ((a * 0.05) + a );
+    }
+    if(operacion === "multiplicacion"){
+        return (a, b) => (a * b );
+    }
+}
+let tax = operaciones("tax");
+alert("El valor del Iva es de " + tax(total_pagar));
 
-alert("El valor del Iva es de " + iva)
+let suma = operaciones("suma");
+const totalmasiva = suma(tax(total_pagar), total_pagar);
+alert("El valor total a pagar es de " + totalmasiva);
 
-const totalmasiva = (a, b) => a + b;
-let totalconiva = (totalmasiva (iva, total_pagar))
+const obsequios = ["viaje", "cena", "carro", "casa"];
+let obsequioelegido = prompt("tienes derecho a elegir uno de los siguientes objetos:   " + obsequios.join("\n")).toLowerCase();
+console.log(obsequioelegido);
+obsequios.includes(obsequioelegido);
 
-alert ("El valor total a pagar es de " + totalconiva)
+if (obsequioelegido === ("viaje")) {
+    alert ("Te ganaste un viaje a la Polinesia Francesa")}
+    if (obsequioelegido === ("cena")) {
+        alert("te ganaste una cena en la torre Eiffel")}
+    if (obsequioelegido === ("carro")) {
+        alert("te ganaste un carro a control remoto")} 
+    if (obsequioelegido === ("casa")) {
+        alert("te ganaste una casa en Alaska")
+    }
 
+let cuotas = prompt ("Ingrese el número de cuotas a diferir el valor")
 
-let nuemerodecuotas = prompt ("Ingrese el número de cuotas a diferir el valor")
-
-if (nuemerodecuotas <= 12) {
-    alert("Has diferido el valor en cuotas que no superan un año, no tendrás intereses!");
+if (cuotas <= 12) {
+    alert("Has diferido el valor en cuotas que no superan un año, no tendrás intereses!")
     } else {
         alert("Has diferido el valor por un término mayor a un año, tus intereses son de 5% sobre el valor en cada cuota");
     } 
 
-const valorcuota = (a,b) => a / b;
-let valorcuotasininteres = (valorcuota(totalconiva,nuemerodecuotas))
-alert ("El valor de la cuota sin intereses es de " + valorcuotasininteres)
+let division = operaciones("division");
+const sininteres = division(totalmasiva,cuotas);
+alert ("El valor de la cuota sin intereses es de " + sininteres);
 
-if (nuemerodecuotas >= 13) {
-const cuotaconinteres = a => ((a * 0.05) + a )
-let cuotamasinteres = cuotaconinteres(valorcuotasininteres)
-alert ("El valor de la cuota con intereses es de " + cuotamasinteres)}
+if (cuotas >= 13) {
+let interes = operaciones("interes");
+const credit = interes(sininteres);
+alert ("El valor de la cuota con intereses es de " + credit)}
 else {
-    alert("El valor de la cuota es de " + valorcuotasininteres)
+    alert("El valor de la cuota es de " + sininteres)
 }
 
-if (nuemerodecuotas >= 13) {
-    const totalventaconinteres = (a, b) => (a * b )
-    let totalfinal = totalventaconinteres(cuotamasinteres, nuemerodecuotas)
-    alert ("El valor total con intereses es de " + totalfinal)}
+if (cuotas >= 13) {
+    let multiplicacion = operaciones("multiplicacion");
+    const totalventa = multiplicacion(credit, cuotas);
+    alert ("El valor total con intereses es de " + totalventa)}
     else {
-        alert("El valor total es de " + totalconiva)
+        alert("El valor total es de " + totalmasiva)
     }
+
+const eliminar = (regalo) => {
+        let posicion = obsequios.indexOf(regalo);
+        if(posicion != -1){
+          obsequios.splice(posicion, 1);
+        }
+      }
+      eliminar(obsequioelegido);
+alert ("Quedan los siguientes obsequios: " + obsequios.join("\n"))
+let ultimoregalo = prompt("tienes derecho a elegir otro de los siguientes objetos:   " + obsequios.join("\n")).toLowerCase();
+obsequios.includes(ultimoregalo);
+
+    if (ultimoregalo === ("viaje")) {
+    alert ("Te ganaste un viaje a la Polinesia Francesa")}
+    if (ultimoregalo === ("cena")) {
+        alert("te ganaste una cena en la torre Eiffel")}
+    if (ultimoregalo === ("carro")) {
+        alert("te ganaste un carro a control remoto")}
+    if (ultimoregalo === ("casa")) {
+        alert("te ganaste una casa en Alaska")}
 
 function myFunction() {
     document.getElementById("demo").innerHTML = "";
     const val = document.getElementById("gastosmensuales").checked;
     console.log(val);
     if(val == true) {
-        alert(name + " " + " el total con iva sin intereses es de compras por Ropa es de " + totalconiva);
+        alert(nombre + " " + " el total con iva sin intereses es de compras por Ropa es de " + totalmasiva);
     } else {
-        alert(name + " " + " el total con iva sin intereses es de compras por alimentacion es de " + totalconiva);
+        alert(nombre + " " + " el total con iva sin intereses es de compras por alimentacion es de " + totalmasiva);
     }
 }
+
+
 alert()
 
