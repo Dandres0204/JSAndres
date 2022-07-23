7a132001c1548b81e3811c91c317ea2945750e09
dandres0204 <daan_andres@hotmail.com>
2022-07-22 21:26:03 -0500

    localstorage

--- index.js
+++ index.js
@@ -29,109 +29,109 @@ for (let i=0; i<cant_objets; i++) {
         break;
     }
 }
-// let mensaje = "El total a pagar sin iva es: $" + total_pagar;
-// alert(mensaje);
+let mensaje = "El total a pagar sin iva es: $" + total_pagar;
+alert(mensaje);
 
-// function operaciones(operacion){
-//     if(operacion === "suma"){
-//         return (x, y) => x + y;
-//     }
-//     if(operacion === "tax"){
-//         return a => a * 0.19;
-//     }
-//     if(operacion === "division"){
-//         return (a,b) => a / b;
-//     }
-//     if(operacion === "interes"){
-//         return  a => ((a * 0.05) + a );
-//     }
-//     if(operacion === "multiplicacion"){
-//         return (a, b) => (a * b );
-//     }
-// }
-// let tax = operaciones("tax");
-// alert("El valor del Iva es de " + tax(total_pagar));
+function operaciones(operacion){
+    if(operacion === "suma"){
+        return (x, y) => x + y;
+    }
+    if(operacion === "tax"){
+        return a => a * 0.19;
+    }
+    if(operacion === "division"){
+        return (a,b) => a / b;
+    }
+    if(operacion === "interes"){
+        return  a => ((a * 0.05) + a );
+    }
+    if(operacion === "multiplicacion"){
+        return (a, b) => (a * b );
+    }
+}
+let tax = operaciones("tax");
+alert("El valor del Iva es de " + tax(total_pagar));
 
-// let suma = operaciones("suma");
-// const totalmasiva = suma(tax(total_pagar), total_pagar);
-// alert("El valor total a pagar es de " + totalmasiva);
+let suma = operaciones("suma");
+const totalmasiva = suma(tax(total_pagar), total_pagar);
+alert("El valor total a pagar es de " + totalmasiva);
 
-// const obsequios = ["viaje", "cena", "carro", "casa"];
-// let obsequioelegido = prompt("tienes derecho a elegir uno de los siguientes objetos:   " + obsequios.join("\n")).toLowerCase();
-// obsequios.includes(obsequioelegido);
+const obsequios = ["viaje", "cena", "carro", "casa"];
+let obsequioelegido = prompt("tienes derecho a elegir uno de los siguientes objetos:   " + obsequios.join("\n")).toLowerCase();
+obsequios.includes(obsequioelegido);
 
-// if (obsequioelegido === ("viaje")) {
-//     alert ("Te ganaste un viaje a la Polinesia Francesa")}
-//     if (obsequioelegido === ("cena")) {
-//         alert("te ganaste una cena en la torre Eiffel")}
-//     if (obsequioelegido === ("carro")) {
-//         alert("te ganaste un carro a control remoto")} 
-//     if (obsequioelegido === ("casa")) {
-//         alert("te ganaste una casa en Alaska")
-//     }
+if (obsequioelegido === ("viaje")) {
+    alert ("Te ganaste un viaje a la Polinesia Francesa")}
+    if (obsequioelegido === ("cena")) {
+        alert("te ganaste una cena en la torre Eiffel")}
+    if (obsequioelegido === ("carro")) {
+        alert("te ganaste un carro a control remoto")} 
+    if (obsequioelegido === ("casa")) {
+        alert("te ganaste una casa en Alaska")
+    }
 
-// let cuotas = prompt ("Ingrese el número de cuotas a diferir el valor")
+let cuotas = prompt ("Ingrese el número de cuotas a diferir el valor")
 
-// if (cuotas <= 12) {
-//     alert("Has diferido el valor en cuotas que no superan un año, no tendrás intereses!")
-//     } else {
-//         alert("Has diferido el valor por un término mayor a un año, tus intereses son de 5% sobre el valor en cada cuota");
-//     } 
+if (cuotas <= 12) {
+    alert("Has diferido el valor en cuotas que no superan un año, no tendrás intereses!")
+    } else {
+        alert("Has diferido el valor por un término mayor a un año, tus intereses son de 5% sobre el valor en cada cuota");
+    } 
 
-// let division = operaciones("division");
-// const sininteres = division(totalmasiva,cuotas);
-// alert ("El valor de la cuota sin intereses es de " + sininteres);
+let division = operaciones("division");
+const sininteres = division(totalmasiva,cuotas);
+alert ("El valor de la cuota sin intereses es de " + sininteres);
 
-// if (cuotas >= 13) {
-// let interes = operaciones("interes");
-// const credit = interes(sininteres);
-// alert ("El valor de la cuota con intereses es de " + credit)}
-// else {
-//     alert("El valor de la cuota es de " + sininteres)
-// }
+if (cuotas >= 13) {
+let interes = operaciones("interes");
+const credit = interes(sininteres);
+alert ("El valor de la cuota con intereses es de " + credit)}
+else {
+    alert("El valor de la cuota es de " + sininteres)
+}
 
-// if (cuotas >= 13) {
-//     let multiplicacion = operaciones("multiplicacion");
-//     const totalventa = multiplicacion(credit, cuotas);
-//     alert ("El valor total con intereses es de " + totalventa)}
-//     else {
-//         alert("El valor total es de " + totalmasiva)
-//     }
+if (cuotas >= 13) {
+    let multiplicacion = operaciones("multiplicacion");
+    const totalventa = multiplicacion(credit, cuotas);
+    alert ("El valor total con intereses es de " + totalventa)}
+    else {
+        alert("El valor total es de " + totalmasiva)
+    }
 
-// const eliminar = (regalo) => {
-//         let posicion = obsequios.indexOf(regalo);
-//         if(posicion != -1){
-//           obsequios.splice(posicion, 1);
-//         }
-//       }
-//       eliminar(obsequioelegido);
-// alert ("Quedan los siguientes obsequios: " + obsequios.join("\n"))
-// let ultimoregalo = prompt("tienes derecho a elegir otro de los siguientes objetos:   " + obsequios.join("\n")).toLowerCase();
-// obsequios.includes(ultimoregalo);
+const eliminar = (regalo) => {
+        let posicion = obsequios.indexOf(regalo);
+        if(posicion != -1){
+          obsequios.splice(posicion, 1);
+        }
+      }
+      eliminar(obsequioelegido);
+alert ("Quedan los siguientes obsequios: " + obsequios.join("\n"))
+let ultimoregalo = prompt("tienes derecho a elegir otro de los siguientes objetos:   " + obsequios.join("\n")).toLowerCase();
+obsequios.includes(ultimoregalo);
 
-//     if (ultimoregalo === ("viaje")) {
-//     alert ("Te ganaste un viaje a la Polinesia Francesa")}
-//     if (ultimoregalo === ("cena")) {
-//         alert("te ganaste una cena en la torre Eiffel")}
-//     if (ultimoregalo === ("carro")) {
-//         alert("te ganaste un carro a control remoto")}
-//     if (ultimoregalo === ("casa")) {
-//         alert("te ganaste una casa en Alaska")}
+    if (ultimoregalo === ("viaje")) {
+    alert ("Te ganaste un viaje a la Polinesia Francesa")}
+    if (ultimoregalo === ("cena")) {
+        alert("te ganaste una cena en la torre Eiffel")}
+    if (ultimoregalo === ("carro")) {
+        alert("te ganaste un carro a control remoto")}
+    if (ultimoregalo === ("casa")) {
+        alert("te ganaste una casa en Alaska")}
 
-// function myFunction() {
-//     document.getElementById("demo").innerHTML = "";
-//     const val = document.getElementById("gastosmensuales").checked;
-//     console.log(val);
-//     if(val == true) {
-//         alert(nombre + " " + " el total con iva sin intereses es de compras por Ropa es de " + totalmasiva);
-//     } else {
-//         alert(nombre + " " + " el total con iva sin intereses es de compras por alimentacion es de " + totalmasiva);
-//     }
-// }
-// let shop = document.createElement("h1");
+function myFunction() {
+    document.getElementById("demo").innerHTML = "";
+    const val = document.getElementById("gastosmensuales").checked;
+    console.log(val);
+    if(val == true) {
+        alert(nombre + " " + " el total con iva sin intereses es de compras por Ropa es de " + totalmasiva);
+    } else {
+        alert(nombre + " " + " el total con iva sin intereses es de compras por alimentacion es de " + totalmasiva);
+    }
+}
+let shop = document.createElement("h1");
 
-// shop.innerHTML = "Tu carrito de compras es el siguiente:";
-// document.body.append(shop);
+shop.innerHTML = "Tu carrito de compras es el siguiente:";
+document.body.append(shop);
 
 
 carrito.forEach(function(element){
@@ -147,3 +147,23 @@ for (var i=0; i< tarjeta.length; i++) {
         this.classList.toggle('blue'); 
       });
     }
+// tarjeta emergeente
+ function OpenModal() {
+    let element = document.getElementById('overlay')
+    element.style.display = 'block'
+  }
+  function CloseModal() {
+    let element = document.getElementById('overlay')
+    element.style.display = 'none'
+  }
+  
+  localStorage.setItem("compras", JSON.stringify(carrito));
+  let compras = localStorage.getItem("compras");
+  let items = JSON.parse(compras);
+
+  items.forEach(function(element){
+  let pop = document.createElement('div')
+    pop.setAttribute("class","pop");
+    pop.textContent = element
+    popup2.appendChild(pop)
+})
